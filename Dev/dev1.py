7187eadf8bf7b9ca7083cd96f882c0520bf08a6f
sumamss153 <65152304+sumamss153@users.noreply.github.com>
2023-06-05 17:14:53 +0530

    Update dev1.py

--- Dev/dev1.py
+++ Dev/dev1.py
@@ -1,30 +1,11 @@
-import pyodbc
+import pandas
+import os
+import time
 
-def main():
-
-    conn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=IN2396424W1\SQLEXPRESS;'
-                      'Database=eRaptor;'
-                      'Trusted_Connection=yes;')
-    tnameS = "testcasecreation_dataquality"
-    checktableexists(conn, tnameS)
-
-def checktableexists(dbcon, tablename):
-
-    dbcur = dbcon.cursor()
-    dbcur.execute("""
-        SELECT COUNT(*)
-        FROM information_schema.tables
-        WHERE table_name = '{0}'
-        """.format(tablename.replace('\'', '\'\'')))
-    exists = dbcur.fetchone()[0]
-    if exists == 1:
-        print("Exists!")
-        dbcur.close()
-        return True
+while True:
+    if os.path.exists("temps_today.csv"):
+        data = pandas.read_csv("temps_today.csv")
+        print(data.mean()["st1"])
     else:
-        print("Not Exists!")
-    dbcur.close()
-    return False
-
-main()
+        print("File not Found!!!")
+    time.sleep(10)
